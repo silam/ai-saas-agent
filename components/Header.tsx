763d7ce3cb582fd1609612e93bebc19d61bf8e83
Si Lam <silam@hotmail.com>
2025-03-01 12:47:56 -0600

    Add style for header on left side

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,14 +6,16 @@ import AgentPulse from './AgentPulse'
 
 function Header() {
   return (
-    <header className='sticky top-0 z-50 left-0 right-0'>
-        <div>
+    <header className='sticky top-0 z-50 left-0 right-0 md:px-0 bg-white/80 backdrop-blur-sm border-gray-200 '>
+        <div className='container mx-auto'>
         {/* left */}
-            <div>
-                <Link href="/">
+            <div className='flex items-center h-16'>
+                <Link href="/" className='flex items-center justify-between gap-4'>
                     {/* Agent pulse */}
                     <AgentPulse size='small' color='blue'></AgentPulse>
-                    <h1>AgentTube</h1>
+                    <h1 className='text-xl font-semibold bg-gradient-to-b
+                     from-blue-600 to-blue-400 bg-clip-text
+                       text-transparent'>AgentTube</h1>
                 </Link>
             </div>
 
